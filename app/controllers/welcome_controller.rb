910b2e9b866440f7c927d1c3c006be3fe42b962c
Saksham Jain <jainsaksham16@gmail.com>
2020-11-08 00:25:53 +0530

    added whatsapp messaging

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,13 +6,20 @@ class WelcomeController < ApplicationController
     account_sid = 'ACa7cc1b6012166c85ee7e06b9dcb4a616' 
     auth_token = '76816cb9bc230638cf48095420491300' 
     @client = Twilio::REST::Client.new(account_sid, auth_token) 
-     
-    message = @client.messages.create( 
-                                 body: 'hello', 
-                                 from: '+12052936261',       
-                                 to: '+917746857774' 
-                               ) 
-     
+    message = 
+      if params[:message] && params[:message][:whatsapp].present?
+        @client.messages.create( 
+                                   body: "#{params[:message][:msg]}", 
+                                   from: 'whatsapp:+14155238886',       
+                                   to: 'whatsapp:+917746857774' 
+                                 ) 
+      else
+       @client.messages.create( 
+                                   body: "#{params[:message][:msg]}", 
+                                   from: '+12052936261',       
+                                   to: '+917746857774' 
+                                 ) 
+      end
   render 'new'
   end
 end
