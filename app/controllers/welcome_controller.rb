7f9b9e3cef061099e79dfa9839945c88d74f2248
Saksham Jain <jainsaksham16@gmail.com>
2020-11-07 23:26:59 +0530

    added twilio

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,19 @@
 class WelcomeController < ApplicationController
+  def new
+  end
+
+  def create
+    account_sid = 'ACa7cc1b6012166c85ee7e06b9dcb4a616' 
+    auth_token = '76816cb9bc230638cf48095420491300' 
+    @client = Twilio::REST::Client.new(account_sid, auth_token) 
+     
+    message = @client.messages.create( 
+                                 body: 'hello', 
+                                 from: '+12052936261',       
+                                 to: '+917746857774' 
+                               ) 
+     
+  render 'new'
+  end
 end
+
